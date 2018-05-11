497310fdf568f57e7f68e4684cb98ec69a1e5c57
BreezySky <pokeforce72@gmail.com>
2018-05-10 23:50:42 -0700

    Tracks "fake" hands and reports positions to server

---
+++ Assets/_Scripts/HandPositionListener.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SharpConnect;
+
+// Sends positions of touch controllers to python script
+public class HandPositionListener : MonoBehaviour {
+    public Connector serverConnection = new Connector();
+    public GameObject rightHand;
+    public GameObject leftHand;
+
+    private enum States { Setup, Playtime };
+    private States currState = States.Setup;
+    private float elapsedTime = 0.0f;
+    
+    // Order is forward, up, back, down, out
+    private Vector3[] leftHandBounds = new Vector3[5];
+    private Vector3[] rightHandBounds = new Vector3[5];
+
+    // Use this for initialization
+    void Start () {
+        // Start server connection
+        Debug.Log(serverConnection.fnConnectResult("localhost", 10000));
+
+        // Put OVRCameraRig and LocalAvatar into scene
+        if (rightHand == null)
+            rightHand = GameObject.Find("LocalAvatar/hand_right");
+        if (leftHand == null)
+            leftHand = GameObject.Find("LocalAvatar/hand_left");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // Report hand positions to Python script once per second
+        if (currState == States.Playtime)
+        {
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime >= 1.0f)
+            {
+                // Log hand positions
+                Debug.Log("Right hand: " + rightHand.transform.position);
+                Debug.Log("Left hand: " + leftHand.transform.position);
+
+                // Send arm positions to server
+                if (serverConnection.isConnected) {
+                    serverConnection.fnPacketTest("MOVE|LARM|" + leftHand.transform.position); 
+                    serverConnection.fnPacketTest("MOVE|RARM|" + rightHand.transform.position); 
+                }
+
+                // Reset timer
+                elapsedTime = 0.0f;
+            }
+        }
+        // Record limits of hands to scale to NAO hands
+        else
+        {
+            if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+            {
+                if (rightHand != null)
+                {
+                    Debug.Log("Right hand: " + rightHand.transform.position);
+                }
+            }
+
+            if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
+            {
+                if (leftHand != null)
+                {
+                    Debug.Log("Left hand: " + leftHand.transform.position);
+                }
+            }
+        }
+    }
+
+    void OnApplicationQuit() {
+        try { serverConnection.fnDisconnect(); } catch { }
+    }
+}
--- Assets/_Scripts/PythonClient.cs
+++ Assets/_Scripts/PythonClient.cs
@@ -5,37 +5,22 @@ using SharpConnect;
 using System.Security.Permissions;
 
 public class PythonClient: MonoBehaviour {
-    public Connector test = new Connector();
+    public Connector serverConnection = new Connector();
     string lastMessage;
     public Transform PlayerCoord;
 
     void Start() {
-        Debug.Log(test.fnConnectResult("Your server IP or URL without HTTP://", 10000, System.Environment.MachineName));
-        if (test.res != "") {
-            Debug.Log(test.res);
-        }
-
+        Debug.Log(serverConnection.fnConnectResult("localhost", 10000));
     }
     void Update() {
+        // 3 arguments for "action": Action|Target|Coordinates
         if (Input.GetKeyDown("space")) {
-            Debug.Log("space key was pressed");
-            test.fnPacketTest("space key was pressed");
+            Debug.Log("space key = 0,0,0");
+            serverConnection.fnPacketTest("MOVE|LARM|" + PlayerCoord.transform.position); 
         }
-
-        if (Input.GetKeyDown("escape")) {
-            Debug.Log("escape key was pressed");
-            test.fnPacketTest("escape key was pressed");
-        }
-        if (test.strMessage != "JOIN") {
-            if (test.res != lastMessage) {
-                Debug.Log(test.res);
-                lastMessage = test.res;
-            }
-        }
-        test.fnPacketTest(PlayerCoord.position[0] + "," + PlayerCoord.position[1] + "," + PlayerCoord.position[2]);
     }
 
     void OnApplicationQuit() {
-        try { test.fnDisconnect(); } catch { }
+        try { serverConnection.fnDisconnect(); } catch { }
     }
 }
