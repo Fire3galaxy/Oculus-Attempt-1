d862ce03852bfc9abc6d16a37ab75f67b8ba65c3
BreezySky <pokeforce72@gmail.com>
2018-06-08 01:07:46 -0700

    Created first person UI with simple display image. Created script that uses B button to swap between them. Tried to make simple display image grabbable

---
+++ Assets/_Scripts/LogicScripts/PerspectiveSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerspectiveSwitcher : MonoBehaviour {
+	public GameObject FirstPersonViews, ThirdPersonViews;
+	public GameObject FirstPersonScreen, ThirdPersonScreen;
+	private RGBPhotoListener cameraScript;
+	
+	// Update is called once per frame
+	void Update () {
+		if (OVRInput.GetDown(OVRInput.RawButton.B, OVRInput.Controller.RTouch) || Input.GetKeyDown("s")) {
+			FirstPersonViews.SetActive(!FirstPersonViews.activeSelf);
+			ThirdPersonViews.SetActive(!ThirdPersonViews.activeSelf);
+
+			if (FirstPersonViews.activeSelf)
+				cameraScript.display = FirstPersonScreen;
+			else
+				cameraScript.display = ThirdPersonScreen;
+		}
+	}
+}
